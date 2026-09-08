8fee804c0184a2e61281db87ed1538560672ef28
Balramog <balrampreet116699@gmail.com>
2026-09-09 02:27:37 +0530

    Time: 0 ms (100%), Space: 10.2 MB (8.52%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -7,27 +7,27 @@ public:
     }
     
     void push(int x) {
-        s1.push(x);
+        s1.push(x);  //directly push in stack 1
         
     }
     
-    int pop() {
-        peek();
-        int frontElement=s2.top();
-        s2.pop();
+    int pop() { //its job is to return and permanently delete the oldest element 
+        peek(); //return the oldest
+        int frontElement=s2.top(); //save in temp 
+        s2.pop(); //permanently remove
         return frontElement;
         
     }
     
     int peek() {
         if(s2.empty()){
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
+            while(!s1.empty()){ //will loop until s1 is empty
+                s2.push(s1.top()); //will push the top element of stack 1 to the stack 2 
+                s1.pop(); //remove one by one
 
             }
         }
-        return s2.top();
+        return s2.top(); //its the top element of stack 2
         
     }
     
