a546efd5a246e3cf873fad8a51ec5938ca802f62
Balramog <balrampreet116699@gmail.com>
2026-09-15 01:34:24 +0530

    Time: 0 ms (100%), Space: 11.3 MB (37.01%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq ;
+            for(char c : s ) freq[c]++;
+            
+            
+    
+        vector<pair<int,char>> vec;
+        for(auto& it : freq){
+            vec.push_back({it.second,it.first});
+        }
+        sort(vec.rbegin(),vec.rend());
+
+        string result;
+        for (auto& p : vec){
+            result.append(p.first,p.second);
+        }
+        return result;
+        
+    }
+};
+
+//--redo
